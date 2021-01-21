16d4a205a8cce6f068cb9032adc0e9a5b6005785
NULL <doodleddd01@gmail.com>
2021-01-21 08:41:22 -0800

    Big Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,75 @@
 
 
-const chars = "1234567890!@#$%^&*()qwertyuiopasdfghjklzxcvbnmQWERATSDFZXCVHBGTYUIOHJKLOPMNB[]"
+var chars = "1234567890!@#$%^&*()qwertyuiopasdfghjklzxcvbnmQWERATSDFZXCVHBGTYUIOHJKLOPMNB[]"
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+async function fancyText(){
+	await sleep(200);
+	for(var i = 0; i < 10; i++){
+		const one = chars[Math.floor(Math.random() * chars.length)];
+		const two = chars[Math.floor(Math.random() * chars.length)];
+		const three = chars[Math.floor(Math.random() * chars.length)];
+		const four = chars[Math.floor(Math.random() * chars.length)];
+		$("#title").text(one+two+three+four);
+		await sleep(50)
+	}
+
+	$("#title").text("N%g*");
+
+	for(var i = 0; i < 10; i++){
+		const two = chars[Math.floor(Math.random() * chars.length)];
+		const three = chars[Math.floor(Math.random() * chars.length)];
+		const four = chars[Math.floor(Math.random() * chars.length)];
+		$("#title").text("N"+two+three+four);
+		await sleep(50)
+	}
+
+	$("#title").text("NUg*");
+
+	for(var i = 0; i < 10; i++){
+		const three = chars[Math.floor(Math.random() * chars.length)];
+		const four = chars[Math.floor(Math.random() * chars.length)];
+		$("#title").text("NU"+three+four);
+		await sleep(50)
+	}
+
+	$("#title").text("NUL*");
+
+	for(var i = 0; i < 10; i++){
+		const four = chars[Math.floor(Math.random() * chars.length)];
+		$("#title").text("NUL"+four);
+		await sleep(50)
+	}
+
+	$("#title").text("NULL");
+}
+
 $(document).ready(function(){
 	
 	// For clicking logo anim
-	
-	$("#logoButton").click(function(){
-		console.log("CLICKED");
-		$("#logo-container").animate({deg: 360}, {duration: 2000, step: function(now){
-			$("#logo-container").css({transform: 'rotate('+now+'deg)'});
-		}})
-	});
 
 
 	// Removes our logo after a user clicks on it for the first time
 
-	$("#logo").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(e){
-		$("#logo").hide();
+	$("#logo-container").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(e){
+		
+		console.log("ANIM ENDED");
 
-		$("#title").show();
-		$("#title").animate({marginLeft: "0px"}, "15s");
+		$("#logoTop").animate({top: "-700px", left: "700px"}, 3000, "swing");
+		$("#bgTop").animate({top: "-700px", left: "700px"}, 3000, "swing");
+		$("#bgBot").animate({top: "700px", left: "-700px"}, 3000, "swing");
+		fancyText();
+		$("#logoBottom").animate({top: "700px", left: "-700px"}, 3000, "swing", function(){
+			$("#logo-container").hide();
+			$("#logoTop").hide();
+			$("#logoBottom").hide();
+			$("#bgBot").hide();
+			$("#bgTop").hide();
+			$("form").show();
+		});
 
 	});
-	const observer = new IntersectionObserver(async function(){
-		for(var i = 0; i < 10; i++){
-			const one = chars[Math.floor(Math.random() * chars.length)];
-			const two = chars[Math.floor(Math.random() * chars.length)];
-			const three = chars[Math.floor(Math.random() * chars.length)];
-			const four = chars[Math.floor(Math.random() * chars.length)];
-			$("#title").text(one+two+three+four);
-			await sleep(50)
-		}
-
-		$("#title").text("N%g*");
-
-		for(var i = 0; i < 10; i++){
-			const two = chars[Math.floor(Math.random() * chars.length)];
-			const three = chars[Math.floor(Math.random() * chars.length)];
-			const four = chars[Math.floor(Math.random() * chars.length)];
-			$("#title").text("N"+two+three+four);
-			await sleep(50)
-		}
-
-		$("#title").text("NUg*");
-
-		for(var i = 0; i < 10; i++){
-			const three = chars[Math.floor(Math.random() * chars.length)];
-			const four = chars[Math.floor(Math.random() * chars.length)];
-			$("#title").text("NU"+three+four);
-			await sleep(50)
-		}
-
-		$("#title").text("NUL*");
-
-		for(var i = 0; i < 10; i++){
-			const four = chars[Math.floor(Math.random() * chars.length)];
-			$("#title").text("NUL"+four);
-			await sleep(50)
-		}
-
-		$("#title").text("NULL");
-	}, {threshold: 0.0})
-
-	observer.observe(document.querySelector('#title'));
 });
